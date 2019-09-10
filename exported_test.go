f516f5093422ef7cdc07dfe6ba7a2f2bfeb14172
Elitsa Pavlova <elitsa.pavlova@ft.com>
2019-09-10 23:21:00 +0300

    Make log field names configurable
    Add the default field names as constants.
    Allow logger initializing with key names' config.

--- exported_test.go
+++ exported_test.go
@@ -18,7 +18,29 @@ func TestWithMonitoringEvent(t *testing.T) {
 	assert.Len(t, hook.LastEntry().Data, 4)
 	assert.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
 	assert.Equal(t, "a info message", hook.LastEntry().Message)
-	assert.Equal(t, "tid_test", hook.LastEntry().Data["transaction_id"])
+	assert.Equal(t, "tid_test", hook.LastEntry().Data[DefaultKeyTransactionID])
+}
+
+func TestWithMonitoringEventWithConf(t *testing.T) {
+	conf := KeyNamesConfig{
+		KeyTransactionID:   "test-transaction-id-key",
+		KeyEventName:       "test-event-name-key",
+		KeyMonitoringEvent: "test-monitoring-key",
+		KeyContentType:     "test-content-type-key",
+	}
+	ulog := NewUPPInfoLogger("test_service", conf)
+	hook := test.NewLocal(ulog.Logger)
+
+	ulog.WithMonitoringEvent("an-event", "tid_test", "some-content").Info("a info message")
+
+	assert.Len(t, hook.Entries, 1)
+	assert.Len(t, hook.LastEntry().Data, 4)
+	assert.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
+	assert.Equal(t, "a info message", hook.LastEntry().Message)
+	assert.Equal(t, "tid_test", hook.LastEntry().Data[conf.KeyTransactionID])
+	assert.Equal(t, "an-event", hook.LastEntry().Data[conf.KeyEventName])
+	assert.Equal(t, "true", hook.LastEntry().Data[conf.KeyMonitoringEvent])
+	assert.Equal(t, "some-content", hook.LastEntry().Data[conf.KeyContentType])
 }
 
 func TestWithTransactionID(t *testing.T) {
@@ -31,7 +53,7 @@ func TestWithTransactionID(t *testing.T) {
 	assert.Len(t, hook.LastEntry().Data, 1)
 	assert.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
 	assert.Equal(t, "a info message", hook.LastEntry().Message)
-	assert.Equal(t, "tid_test", hook.LastEntry().Data["transaction_id"])
+	assert.Equal(t, "tid_test", hook.LastEntry().Data[DefaultKeyTransactionID])
 }
 
 func TestWithCategorisedEvent(t *testing.T) {
@@ -44,8 +66,8 @@ func TestWithCategorisedEvent(t *testing.T) {
 	assert.Len(t, hook.LastEntry().Data, 4)
 	assert.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
 	assert.Equal(t, "a info message", hook.LastEntry().Message)
-	assert.Equal(t, "tid_test", hook.LastEntry().Data["transaction_id"])
-	assert.Equal(t, "an-event", hook.LastEntry().Data["event"])
-	assert.Equal(t, "an-event-category", hook.LastEntry().Data["event_category"])
-	assert.Equal(t, "an-event-msg", hook.LastEntry().Data["event_msg"])
+	assert.Equal(t, "tid_test", hook.LastEntry().Data[DefaultKeyTransactionID])
+	assert.Equal(t, "an-event", hook.LastEntry().Data[DefaultKeyEventName])
+	assert.Equal(t, "an-event-category", hook.LastEntry().Data[DefaultKeyEventCategory])
+	assert.Equal(t, "an-event-msg", hook.LastEntry().Data[DefaultKeyEventMsg])
 }
